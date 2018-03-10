d0a179f8d0fe71b915fa3c1a7bacfe16f87ffc06
Jelambrar96 <jelambrar@gmail.com>
2018-03-10 18:23:53 -0500

    firts_commit

---
+++ apps/position_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PositionDataConfig(AppConfig):
+    name = 'position_data'
---
+++ apps/position_data/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Position_data(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField() 
+    datetime = models.DateTimeField() 
+    velocity = models.FloatField(default = 0)
+    id_syrus = models.CharField(max_length = 16, default = '')
+    
