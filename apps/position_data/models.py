7eaa90377bb3d43180a07647930c06abce4ac4f6
Jelambrar96 <jelambrar@gmail.com>
2018-03-13 16:23:30 -0500

    commit05

--- apps/position_data/models.py
+++ apps/position_data/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Position_data(models.Model):
     latitude = models.FloatField()
-    longitude = models.FloatField() 
-    datetime = models.DateTimeField() 
+    longitude = models.FloatField()
+    datetime = models.DateTimeField()
     velocity = models.FloatField(default = 0)
     id_syrus = models.CharField(max_length = 16, default = '')
-    
+
--- syrus_project/settings.py
+++ syrus_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.position_data',
+    'apps.user',
+    'apps.vehicle',
 ]
 
 MIDDLEWARE = [
