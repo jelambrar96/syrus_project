df6135d9d1071c9aec4cd0dda889d2e4e3934cfb
Jelambrar96 <jelambrar@gmail.com>
2018-03-14 17:57:08 -0500

    commit_04

--- syrus_project/wsgi.py
+++ syrus_project/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys 
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/syrus_project')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "syrus_project.settings")
 
