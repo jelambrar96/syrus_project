a361dcd0bea607dcfb5754f7afe86d8fdc03b4ea
jelambrar96 <jelambrar@gmail.com>
2018-03-26 20:16:16 -0500

    commit

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -16,15 +16,15 @@ import pymysql
 import datetime
 #import sniffer
 
-TAB_1 = '\t - '
-TAB_2 = '\t\t - '
-TAB_3 = '\t\t\t - '
-TAB_4 = '\t\t\t\t - '
+#TAB_1 = '\t - '
+#TAB_2 = '\t\t - '
+#TAB_3 = '\t\t\t - '
+#TAB_4 = '\t\t\t\t - '
 
-DATA_TAB_1 = '\t '
-DATA_TAB_2 = '\t\t '
-DATA_TAB_3 = '\t\t\t '
-DATA_TAB_4 = '\t\t\t\t '
+#DATA_TAB_1 = '\t '
+#DATA_TAB_2 = '\t\t '
+#DATA_TAB_3 = '\t\t\t '
+#DATA_TAB_4 = '\t\t\t\t '
 
 #................................................................
 # this depends of our amazon instance
