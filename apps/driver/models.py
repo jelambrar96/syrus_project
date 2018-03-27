53f06cc48c5e85e8d220876d65b40ee0fa7c1bb2
jelambrar96 <jelambrar@gmail.com>
2018-03-26 23:28:48 -0500

    commit

--- apps/driver/models.py
+++ apps/driver/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model):
+    driver_name = models.CharField(max_length = 24, default = '')
+    driver_lastname = models.CharField(max_length = 24, default = '')
+    id_syrus = models.CharField(max_length = 16, default = '')
+    driver_email = models.EmailField()
+    driver_ciu = models.IntegerField()
+    drive_tel = models.CharField(max_length = 16, default = '')
+
+"""
+class Position_data(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    datetime = models.DateTimeField()
+    velocity = models.FloatField(default = 0)
+    id_syrus = models.CharField(max_length = 16, default = '')
+"""
--- apps/user_login/models.py
+++ apps/user_login/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length = 24, default = '')
+    user_lastname = models.CharField(max_length = 24, default = '')
+    user_email = models.EmailField()
+    drive_tel = models.CharField(max_length = 16, default = '')
