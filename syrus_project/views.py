298ffc1f86f6a85f9779d0067041b5a4db70c0df
jelambrar96 <jelambrar@gmail.com>
2018-04-01 12:36:13 -0500

    commit

--- syrus_project/views.py
+++ syrus_project/views.py
@@ -1,8 +1,10 @@
 import datetime 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render 
 
 def Hola(request):
-    return HttpResponse("HolaMundo")
+    #return HttpResponse("HolaMundo")
+    return render(request, 'holamundo.html')
 
 def Datetime(request):
     now = datetime.datetime.now()
@@ -13,7 +15,7 @@ def Hours_before(request, offset):
     try:
         offset = int(offset)
     except ValueError:
-        raise Http404()
+        raise Http404
     dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
     html = '<html><body><h1>En %s Hora(s) seran: </h1><h3>%s</h3></body></html>' %(offset, dt)
     return HttpResponse(html)
