ea4fd2fb0d6df2f0d8b54790954ccdd910af9591
jelambrar96 <jelambrar@gmail.com>
2018-03-31 23:15:11 -0500

    commit

--- apps/driver/models.py
+++ apps/driver/models.py
@@ -5,13 +5,14 @@ from django.db import models
 
 # Create your models here.
 class Driver(models.Model):
-    driver_name = models.CharField(max_length = 24, default = '')
-    driver_lastname = models.CharField(max_length = 24, default = '')
-    id_syrus = models.CharField(max_length = 16, default = '')
+    driver_name = models.CharField(max_length = 24, blank = False)
+    driver_password = models.CharField(max_length = 48, blank = False)
+    driver_lastname = models.CharField(max_length = 24, blank = False)
+    id_syrus = models.CharField(max_length = 16, blank = False)
     driver_email = models.EmailField()
     driver_ciu = models.IntegerField()
-    drive_tel = models.CharField(max_length = 16, default = '')
-    driver_password = models.CharField(max_length = 48)
+    drive_tel = models.CharField(max_length = 16, blank = False)
+    
 
     def __str__(self): 
         return '%s %s' %(self.driver_name, self.driver_lastname)
--- apps/user_login/models.py
+++ apps/user_login/models.py
@@ -6,11 +6,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user_username = models.CharField(max_length = 12, blank = False)
+    user_password = models.CharField(max_length = 48, blank = False)
     user_name = models.CharField(max_length = 24, blank = False)
     user_lastname = models.CharField(max_length = 24, blank = False)
     user_email = models.EmailField(blank = False)
     user_tel = models.CharField(max_length = 16, default = '')
-    user_password = models.CharField(max_length = 48, blank = False)
+    
 
     def __str__(self):
         return self.user_name
