35d2bb0ee4457e894f1480ed00c2917c00bf099b
jelambrar96 <jelambrar@gmail.com>
2018-03-31 22:32:29 -0500

    commit

--- apps/driver/admin.py
+++ apps/driver/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from apps.driver.models import Driver
 
 # Register your models here.
+class DriverAdmin(admin.ModelAdmin):
+    pass 
+
+admin.site.register(Driver, DriverAdmin)
