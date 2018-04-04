4907acf38f3c3b4556567a2ae3d714de494e4091
Jelambrar96 <jelambrar@gmail.com>
2018-04-03 19:45:28 -0500

    commit

--- syrus_project/settings.py
+++ syrus_project/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 
 #change static folter
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_bootstrap'),
     #'/var/www/syrus_project/static/',
 ]
