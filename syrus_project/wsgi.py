e63a3be459711330b7fedd1abf2c1b030e422d10
Jelambrar96 <jelambrar@gmail.com>
2018-04-10 00:14:13 -0500

    commit

--- syrus_project/wsgi.py
+++ syrus_project/wsgi.py
@@ -10,10 +10,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 import sys 
 
+path1 = os.getcwd()
+path2 = path1 + '/syrus_proyect'
+
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/var/www/syrus_project')
-sys.path.append('/var/www/syrus_project/syrus_project')
+#sys.path.append('/var/www/syrus_project')
+#sys.path.append('/var/www/syrus_project/syrus_project')
+
+sys.path.append(path1)
+sys.path.append(path2)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "syrus_project.settings")
 
