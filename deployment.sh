d9711f9b051f6dfec88f32b1076219916aa216e3
Jelambrar96 <jelambrar@gmail.com>
2018-04-09 23:33:03 -0500

    changes

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,5 @@
 #!bin/sh
-cd /var/www/syrus_project
+cd syrus_project/
 git pull origin master
 sudo service apache2 restart
+
--- syrus_project/settings.py
+++ syrus_project/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 #change static folter
 STATICFILES_DIRS = [
