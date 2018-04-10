1a65538bf3b4132e6d150b5c7da22c2e1de71044
Jelambrar96 <jelambrar@gmail.com>
2018-04-09 23:35:49 -0500

    c

--- syrus_project/settings.py
+++ syrus_project/settings.py
@@ -124,13 +124,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-#change static folter
+#change static folter 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
